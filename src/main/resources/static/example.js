7f1e8f19a7dc168839b7eb1673110dd7c4cf3cad
Benjamin Winterberg <winterbe@googlemail.com>
2015-02-02 19:57:27 +0100

    React tutorial

--- src/main/resources/static/example.js
+++ src/main/resources/static/example.js
@@ -24,10 +24,16 @@ var Comment = React.createClass({
 
 var CommentList = React.createClass({
     render: function () {
+        var commentNodes = this.props.data.map(function (comment) {
+            return (
+                <Comment author={comment.author}>
+                    {comment.text}
+                </Comment>
+            );
+        });
         return (
             <div className="commentList">
-                <Comment author="Peter Parker">This is a **comment**.</Comment>
-                <Comment author="John Doe">This is *another* comment.</Comment>
+                {commentNodes}
             </div>
         );
     }
@@ -38,14 +44,19 @@ var CommentBox = React.createClass({
         return (
             <div className="commentBox">
                 <h1>Comments</h1>
-                <CommentList />
+                <CommentList data={this.props.data} />
                 <CommentForm />
             </div>
         );
     }
 });
 
+var data = [
+    {author: "Pete Hunt", text: "This is one comment"},
+    {author: "Jordan Walke", text: "This is *another* comment"}
+];
+
 React.render(
-    <CommentBox />,
+    <CommentBox data={data} />,
     document.getElementById("content")
 );
